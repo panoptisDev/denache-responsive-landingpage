26e99df6735b8e66c508f7cc0f072960ee138796
Maxim Buz <mbuz.maxim@gmail.com>
2022-02-09 18:32:46 +0100

    extracted hero section to seperate component

---
+++ components/HeroSection.js
@@ -0,0 +1,80 @@
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+import styled from 'styled-components';
+import ContactButton from '../components/ContactButton';
+
+import hand from '../public/hand-1.png';
+import VioletEllipse from '../components/VioletEllipse';
+
+
+const Section = styled.section`
+ display: flex;
+ align-items: center;
+ min-height: 100vh;
+ max-height: 100vh;
+
+ @media (min-width: 64rem) {
+  width: 56rem;
+  margin: 0 auto;
+ }
+
+ @media (min-width: 34rem) {
+  margin: 0 auto;
+ }
+`;
+
+const HeroTextWrapper = styled(motion.div)`
+ flex: 1 1;
+ display: flex;
+ flex-direction: column;
+ gap: 20px;
+`;
+
+/* Animation */
+const HeroTextWrapperVariants = {
+ hidden: { opacity: 0, x: -100, rotate: 20 },
+ show: {
+  opacity: 1,
+  x: 0,
+  rotate: 0,
+  transition: {
+   delay: 0.3,
+   staggerChildren: 10,
+  },
+ },
+};
+
+/* Animation */
+const HeroTextItem = {
+ hidden: { opacity: 0, x: -100 },
+ show: { opacity: 1, x: 0 },
+};
+
+const HeroImageWrapper = styled(motion.div)`
+ 
+`;
+
+const HandImageWrapper = styled(motion.div)`
+ float: right;
+`;
+
+export default function HeroSection() {
+  return (
+    <Section>
+     <HeroTextWrapper variants={HeroTextWrapperVariants} initial='hidden' animate='show'>
+      <motion.h1 variants={HeroTextItem}>Smart, creative development.</motion.h1>
+      <motion.p>
+       No techie? No problem. I provide creative and professional software development for your business
+       vision.
+      </motion.p>
+      <ContactButton variants={HeroTextItem} text='Talk to me' url={'/'} size='10px 28px' float='left' />
+     </HeroTextWrapper>
+     <HeroImageWrapper>
+      <HandImageWrapper>
+        <VioletEllipse/>
+        <Image alt='3D-Hand' src={hand} height={400} width={260}></Image>
+      </HandImageWrapper>
+     </HeroImageWrapper>
+    </Section>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,64 +1,9 @@
 import { motion } from 'framer-motion';
 import Head from 'next/head';
-import Image from 'next/image';
-import styled from 'styled-components';
+
 
 import NavigationBar from '../components/NavigationBar';
-import ContactButton from '../components/ContactButton';
-
-import hand from '../public/hand-1.png';
-import VioletEllipse from '../components/VioletEllipse';
-
-const HeroSection = styled.section`
- display: flex;
- align-items: center;
- min-height: 100vh;
- max-height: 100vh;
-
- @media (min-width: 64rem) {
-  width: 56rem;
-  margin: 0 auto;
- }
-
- @media (min-width: 34rem) {
-  margin: 0 auto;
- }
-`;
-
-const HeroTextWrapper = styled(motion.div)`
- flex: 1 1;
- display: flex;
- flex-direction: column;
- gap: 20px;
-`;
-
-/* Animation */
-const HeroTextWrapperVariants = {
- hidden: { opacity: 0, x: -100, rotate: 20 },
- show: {
-  opacity: 1,
-  x: 0,
-  rotate: 0,
-  transition: {
-   delay: 0.3,
-   staggerChildren: 10,
-  },
- },
-};
-
-/* Animation */
-const HeroTextItem = {
- hidden: { opacity: 0, x: -100 },
- show: { opacity: 1, x: 0 },
-};
-
-const HeroImageWrapper = styled(motion.div)`
- 
-`;
-
-const HandImageWrapper = styled(motion.div)`
- float: right;
-`;
+import HeroSection from '../components/HeroSection';
 
 export default function Home() {
  return (
@@ -70,22 +15,7 @@ export default function Home() {
 
    <main>
     <NavigationBar />
-    <HeroSection>
-     <HeroTextWrapper variants={HeroTextWrapperVariants} initial='hidden' animate='show'>
-      <motion.h1 variants={HeroTextItem}>Smart, creative development.</motion.h1>
-      <motion.p>
-       No techie? No problem. I provide creative and professional software development for your business
-       vision.
-      </motion.p>
-      <ContactButton variants={HeroTextItem} text='Talk to me' url={'/'} size='10px 28px' float='left' />
-     </HeroTextWrapper>
-     <HeroImageWrapper>
-      <HandImageWrapper>
-        <VioletEllipse/>
-        <Image alt='3D-Hand' src={hand} height={400} width={260}></Image>
-      </HandImageWrapper>
-     </HeroImageWrapper>
-    </HeroSection>
+    <HeroSection/>
    </main>
 
    <footer></footer>
